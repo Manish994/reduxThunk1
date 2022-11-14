73a43e999266631be2b65546f7557bd72f362cca
Manish <manish@gmail.com>
2022-11-14 20:18:51 +0545

    Next part is going on createSlice

--- src/function/index.js
+++ src/function/index.js
@@ -10,15 +10,15 @@
 import { createAction } from "@reduxjs/toolkit";
 
 //by default return
-// {type:"UPDATE_LOCATION", payload: value passed from component}
-// {type:"UPDATE_AGE", payload: value passed from component}
+// {type:"UPDATE_LOCATION", payload: value passed from component (function => index)}
+// {type:"UPDATE_AGE", payload: value passed from component (function => index)}
 
 const locationCurrent = createAction("UPDATE_LOCATION");
 const locationAge = createAction("UPDATE_AGE");
 const fetchName = () => {
   return async (dispatch) => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    debugger;
+
     const result = await res.json();
     dispatch({ type: "UPDATE_NAME", payload: result[0].name });
   };
